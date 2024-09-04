2b9ac813d01409195ad33bddcf3a303f2df34066
muhammadzulfans <muhammadzulfans932@gmail.com>
2024-09-04 20:06:48 +0700

    Notes Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./assets/Component/Login/Login";
+// import { Route, Routes } from "react-router-dom";
+// import Login from "./assets/Component/Login/Login";
+// import Home from "./assets/Component/Home/Home";
+
+import Notes from "./assets/Component/File Notes/Notes";
 import Home from "./assets/Component/Home/Home";
+import Login from "./assets/Component/Login/Login";
+
 
 function App() {
      return (
-          <Routes>
-               <Route path="./assets/Component/Login/Login.tsx" element={<Login />} />
-               <Route path="./assets/Component/Home/Home.tsx" element={<Home />} />
-          </Routes>
-     );
-}
+     //      <Routes>
+     //           <Route path="./assets/Component/Login/Login.tsx" element={<Login />} />
+     //           <Route path="./assets/Component/Home/Home.tsx" element={<Home />} />
+     //      </Routes>
+     // );
+     <div>
+          <Login/>
+          <Home/>
+          <Notes/>
+     </div>
+)}
 
 export default App;
---
+++ src/assets/Component/File Notes/Notes.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Notes: React.FC = () => {
+     return (
+          <div className="bg-orange-300 h-screen">
+               <div className="text-4xl p-16 font-mono">
+                    <div className="flex justify-between grid-rows-2 px-16">
+                         <button>
+                              <img src="/src/assets/left-arrow.png" className="w-20" />
+                         </button>
+                         <button>
+                              <img src="/src/assets/check.png" className="w-24" />
+                         </button>
+                    </div>
+                    <form>
+                         <div className="px-20 pt-12">
+                              <input type="text" className="text-7xl bg-transparent border-b-2 border-orange-700 w-full py-2 px-3 text-black placeholder-gray-500 focus:outline-none" placeholder="Judul" />
+                         </div>
+                         <div className="px-20 pt-14">
+                              <textarea className="text-4xl w-full py-2 px-3 text-black focus:outline-none bg-transparent placeholder-gray-500 h-full" placeholder="Mulai mengetik..."></textarea>
+                         </div>
+                    </form>
+               </div>
+          </div>
+     );
+};
+
+export default Notes;
